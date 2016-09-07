2e4419fe40319e0028705bd18fd29a7881420aee
dams2007 <damien.bigot@gmail.com>
2016-09-07 12:50:17 +0900

    display all pokemon on screen

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -8,18 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout  {
 
+  @IBOutlet weak var collectionView :UICollectionView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    collectionView.delegate = self
+    collectionView.dataSource = self
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    
+    if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell {
+      
+      print(indexPath)
+      
+      let pokemon = Pokemon(name: "test", pokedexId: indexPath.row)
+      
+      cell.configureCell(pokemon)
+      
+      return cell
+      
+    } else {
+      return UICollectionViewCell()
+    }
+    
   }
-
-
+  
+  
+  
+  
+  
+  
+  
+  
+  func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 718
+  }
+  
+  func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+    return 1
+  }
+  
+  func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+    return CGSizeMake(105, 105)
+  }
+  
+  func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath){
+    
+  }
 }
 
