465c008f398f693d7b814fc8144b6056298ea4c8
dams2007 <damien.bigot@gmail.com>
2016-09-25 22:46:24 +0900

    connect data to the detailViewController

--- Pokedex/DetailViewController.swift
+++ Pokedex/DetailViewController.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
     var pokemon: Pokemon!
-  
+    
     @IBOutlet weak var pokemonName: UILabel!
     @IBOutlet weak var segmentControl: UISegmentedControl!
     @IBOutlet weak var pokemonImage: UIImageView!
@@ -28,15 +28,44 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        
         pokemonName.text = pokemon.name
         pokemonImage.image = UIImage(named: "\(pokemon.pokedexId)")
+        firstEvolutionImage.image = UIImage(named: "\(pokemon.pokedexId)")
         
-        pokemon.downloadPokemonDetails { 
+        pokemon.downloadPokemonDetails {
             // this will be called after download is done
-            
+            self.updateUI()
         }
     }
+    
+    func updateUI() {
+        descriptionLabel.text = pokemon.description
+        typeLabel.text = pokemon.type
+        defenseLabel.text = pokemon.defense
+        heightLabel.text = pokemon.height
+        idLabel.text = "\(pokemon.pokedexId)"
+        weightLabel.text = pokemon.weight
+        attackLabel.text = pokemon.attack
+        
+        if pokemon.nextEvolutionId == "" {
+            evolutionLabel.text = "No Evolutions"
+            secondEvolutionImage.isHidden = true
+        } else {
+            secondEvolutionImage.isHidden = false
+            print(pokemon.nextEvolutionId)
+            secondEvolutionImage.image = UIImage(named: pokemon.nextEvolutionId)
+            var str = "Next Evolution: \(pokemon.nextEvolutionTxt)"
+            
+            
+            if pokemon.nextEvolutionLvl != "" {
+                str += " - LVL \(pokemon.nextEvolutionLvl)"
+            }
+            evolutionLabel.text = str
+        }
+        
+    }
+    
     @IBAction func dismissDetailViewController(_ sender: AnyObject) {
         dismiss(animated: true, completion: nil)
     }
